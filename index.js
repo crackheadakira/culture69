45cd60989562e064e652820efa8e12a54fdbd55e
crackheadakira <lasn.mine@gmail.com>
2022-07-20 19:42:01 +0200

    Fix FS directory

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 module.exports = function () {
-    const database = JSON.parse(fs.readFileSync('./db.json'));
+    const database = JSON.parse(fs.readFileSync(path.join(__dirname, './db.json')));
     return database[getRandom(0, database.length)];
 
 }
